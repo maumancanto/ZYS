76ef88ab050ef26af7afe5d0c94165f5f291f68e
alanucan96 <alanvucan@gmail.com>
2019-06-13 17:28:38 -0500

    Success

--- ZYS/ZYS/App_Start/RouteConfig.cs
+++ ZYS/ZYS/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace ZYS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ZYS", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
